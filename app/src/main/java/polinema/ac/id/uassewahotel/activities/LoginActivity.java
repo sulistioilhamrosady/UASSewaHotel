99718f4f2c13875bdae42f79ce3f4df1d0e3de50
Tio Rosady <sulistioilhamrosady@gmail.com>
2019-12-06 00:24:36 +0700

    login

---
+++ app/src/main/java/polinema/ac/id/uassewahotel/activities/LoginActivity.java
@@ -0,0 +1,28 @@
+package polinema.ac.id.uassewahotel.activities;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import polinema.ac.id.uassewahotel.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+}
--- app/src/main/java/polinema/ac/id/uassewahotel/fragment/AccountFragment.java
+++ app/src/main/java/polinema/ac/id/uassewahotel/fragment/AccountFragment.java
@@ -1,6 +1,7 @@
 package polinema.ac.id.uassewahotel.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,8 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import polinema.ac.id.uassewahotel.R;
+import polinema.ac.id.uassewahotel.activities.LoginActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,7 +29,26 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        Button Login = (Button) view.findViewById(R.id.login);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getActivity(), LoginActivity.class);
+                startActivity(in);
+            }
+        });
+
+        Button registrasi = (Button) view.findViewById(R.id.registrasi);
+        registrasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getActivity(), LoginActivity.class);
+                startActivity(in);
+            }
+        });
+        return view;
     }
 
 }
